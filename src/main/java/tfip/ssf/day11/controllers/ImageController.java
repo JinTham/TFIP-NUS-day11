0bd07208dae6dfb7f810e2df99fe9ca5e3b8d643
Yeh Jin Tham <yehjintham@gmail.com>
2023-02-06 16:08:39 +0800

    commit

--- src/main/java/tfip/ssf/day11/controllers/ImageController.java
+++ src/main/java/tfip/ssf/day11/controllers/ImageController.java
@@ -1,6 +1,9 @@
 package tfip.ssf.day11.controllers;
 
+import java.lang.System.Logger.Level;
 import java.util.Random;
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,20 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 //Tell Spring Boot this is the controller
 @Controller
-@RequestMapping(path = {"/randomimage"}) //Give the address link a path name, it is not referring to the html file name
+@RequestMapping(path = {"/randomimage"},produces = MediaType.TEXT_HTML_VALUE) //Give the address link a path name, it is not referring to the html file name
 public class ImageController {
+    //Setup a logger
+    private Logger logger = Logger.getLogger(NumberController.class.getName());
     //GET /randomimage
     @GetMapping
     public String getRandomImage(Model model){
         Random rand = new Random();
         int randomNum = rand.nextInt(31);
-        //String randomNumStr = "number"+Integer.toString(randomNum);
-
-        System.out.printf("The random num is %s\n",randomNum);
-
-        //Add the time to the model
+        //Create a log format called imgIndex
+        logger.log(Level.INFO,"imgIndex = %d".formatted(randomNum));
+        //Bind the value to the model
         model.addAttribute("randomImage", randomNum);
-        //Return the view as (randomImage.html)
+        //Render the template with the model
         return "randomImage";
     }
 }
