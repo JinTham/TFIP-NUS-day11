2eb517e4ca475c70651d5c3729fd28309d505001
Yeh Jin Tham <yehjintham@gmail.com>
2023-02-07 09:12:06 +0800

    commit

--- src/main/java/tfip/ssf/day11/controllers/ImageController.java
+++ src/main/java/tfip/ssf/day11/controllers/ImageController.java
@@ -1,6 +1,6 @@
 package tfip.ssf.day11.controllers;
 
-import java.lang.System.Logger.Level;
+import java.util.logging.Level;
 import java.util.Random;
 import java.util.logging.Logger;
 
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.MediaType;
 
 //Tell Spring Boot this is the controller
 @Controller
 @RequestMapping(path = {"/randomimage"},produces = MediaType.TEXT_HTML_VALUE) //Give the address link a path name, it is not referring to the html file name
 public class ImageController {
     //Setup a logger
-    private Logger logger = Logger.getLogger(NumberController.class.getName());
+    private Logger logger = Logger.getLogger(ImageController.class.getName());
     //GET /randomimage
     @GetMapping
     public String getRandomImage(Model model){
